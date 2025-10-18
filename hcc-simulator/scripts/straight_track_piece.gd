4e86c8decae7082452656355fb43a137d37c0ecd
KylerLimata <kyler.limata@gmail.com>
2025-10-18 13:45:05 -0700

    Fixed buggy straight track pieces + z forward
    Track pieces now assemble their own mesh, fixing issues with uniqueness & now use z as the forward axis

--- hcc-simulator/scripts/straight_track_piece.gd
+++ hcc-simulator/scripts/straight_track_piece.gd
@@ -1,37 +1,52 @@
 @tool
 extends Node3D
 
-
-# EDITOR PROPERTIES
 @export_group("Track")
 @export_range(1.0, 100.0, 1.0, "suffix:m") var length: float = 1.0 : set = set_length
+
+var queued_ground_array = null
+var queued_left_wall_array = null
+var queued_right_wall_array = null
 
 func set_length(val: float):
 	length = val
 	update_mesh()
 
+func _process(delta: float) -> void:
+	if queued_ground_array != null:
+		$Ground/CollisionPolygon3D.polygon = queued_ground_array
+		$Ground/CSGPolygon3D.polygon = queued_ground_array
+		queued_ground_array = null
+	
+	if queued_left_wall_array != null:
+		$LeftWall/CollisionPolygon3D.polygon = queued_left_wall_array
+		$LeftWall/CSGPolygon3D.polygon = queued_left_wall_array
+		queued_left_wall_array = null
+		
+	if queued_right_wall_array != null:
+		$RightWall/CollisionPolygon3D.polygon = queued_right_wall_array
+		$RightWall/CSGPolygon3D.polygon = queued_right_wall_array
+		queued_right_wall_array = null
+
 func update_mesh():
-	var ground_shape: BoxShape3D = $Ground/CollisionShape3D.shape
-	ground_shape.size.x = length
-	
-	var ground_mesh: BoxMesh = $Ground/MeshInstance3D.mesh
-	ground_mesh.size.x = length
-	
-	var left_wall_shape: BoxShape3D = $"Left Wall/CollisionShape3D".shape
-	left_wall_shape.size.x = length
-	
-	var left_wall_mesh: BoxMesh = $"Left Wall/MeshInstance3D".mesh
-	left_wall_mesh.size.x = length
-	
-	var right_wall_shape: BoxShape3D = $"Right Wall/CollisionShape3D".shape
-	right_wall_shape.size.x = length
-	
-	var right_wall_mesh: BoxMesh = $"Right Wall/MeshInstance3D".mesh
-	right_wall_mesh.size.x = length
-	
-	$Ground.position.x = length/2
-	$"Left Wall".position.x = length/2
-	$"Right Wall".position.x = length/2
+	queued_ground_array = [
+		Vector2(-2.0, 0.0),
+		Vector2(-2.0, length),
+		Vector2(2.0, length),
+		Vector2(2.0, 0.0)
+	]
+	queued_left_wall_array = [
+		Vector2(-2.0, 0.0),
+		Vector2(-2.0, length),
+		Vector2(-1.75, length),
+		Vector2(-1.75, 0.0)
+	]
+	queued_right_wall_array = [
+		Vector2(2.0, 0.0),
+		Vector2(2.0, length),
+		Vector2(1.75, length),
+		Vector2(1.75, 0.0)
+	]
 
 func get_end_transform():
-	return Transform3D(Basis(), Vector3(length, 0, 0))
+	return Transform3D(Basis(), Vector3(0, 0, length))
