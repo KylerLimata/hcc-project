9f904f513cab48f6ac0ababbf68c12e958a4afa2
KylerLimata <kyler.limata@gmail.com>
2025-11-07 16:34:48 -0800

    Update network each episode

--- hcc-simulator/hello.py
+++ hcc-simulator/hello.py
@@ -147,7 +147,62 @@ while episode_count < max_episodes:
         for i in range(len(checkpoint_times)):
             baseline_time = baseline_checkpoint_times[i]
             nn_time = checkpoint_times[i]
+            reward = nn_time - baseline_time
 
-            episode_reward += nn_time - baseline_time
+            # Reward every step until it hits the checkpoint
+            while len(rewards_history) < nn_time:
+                rewards_history.append(reward)
+
+            episode_reward += reward
+
+        # Update running reward to check condition for solving
+        running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
     
-    episode_count += 1
+        episode_count += 1
+
+        # Calculate expected value from rewards
+        # - At each timestep what was the total reward received after that timestep
+        # - Rewards in the past are discounted by multiplying them with gamma
+        # - These are the labels for our critic
+        returns = []
+        discounted_sum = 0
+        for r in rewards_history[::-1]:
+            discounted_sum = r + gamma * discounted_sum
+            returns.insert(0, discounted_sum)
+    
+        # Normalize
+        returns = np.array(returns)
+        returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
+        returns = returns.tolist()
+
+        # Calculating loss values to update our network
+        history = zip(steering_action_probs_history, engine_action_probs_history, critic_value_history, returns)
+        actor_losses = []
+        critic_losses = []
+        for log_prob_steering, log_prob_engine, value, ret in history:
+            # At this point in history, the critic estimated that we would get a
+            # total reward = `value` in the future. We took an action with log probability
+            # of `log_prob` and ended up receiving a total reward = `ret`.
+            # The actor must be updated so that it predicts an action that leads to
+            # high rewards (compared to critic's estimate) with high probability.
+            diff = ret - value
+            log_prob = log_prob_steering + log_prob_engine
+            actor_losses.append(-log_prob * diff)  # actor loss
+            # The critic must be updated so that it predicts a better estimate of
+            # the future rewards.
+            critic_losses.append(
+                huber_loss(ops.expand_dims(value, 0), ops.expand_dims(ret, 0))
+            )
+        
+        # Backpropagation
+        loss_value = sum(actor_losses) + sum(critic_losses)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        # Clear the loss and reward history
+        steering_action_probs_history.clear()
+        engine_action_probs_history.clear()
+        critic_value_history.clear()
+        rewards_history.clear()
+
+    sim.print(f" reward: {episode_reward}")
