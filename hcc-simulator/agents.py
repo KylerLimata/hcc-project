da56d8b943003bbf0c926dbd1b5ecc5704ad8f8a
KylerLimata <kyler.limata@gmail.com>
2025-12-02 10:23:08 -0800

    Steering-only training

--- hcc-simulator/agents.py
+++ hcc-simulator/agents.py
@@ -149,3 +149,219 @@ class NNAgent:
 
         return [engine_power, steering_power]
     
+class NNEngineAgent:
+    def __init__(
+            self, 
+            model, 
+            num_steering_actions: int, 
+            num_engine_actions: int
+            ):
+        import tensorflow as tf
+        import numpy as np
+        # Upack model weights
+        # Weights evaluated with numpy for performance
+        layers = model.layers
+
+        self.hidden_layers = []
+        for layer in layers:
+            if isinstance(layer, tf.keras.layers.Dense) and "out" not in layer.name:
+                weights = layer.get_weights()
+                if len(weights) == 2:
+                    self.hidden_layers.append((weights[0], weights[1], layer.activation))
+
+        self.output_layers = []
+        for layer in layers:
+            if isinstance(layer, tf.keras.layers.Dense) and "out" in layer.name:
+                weights = layer.get_weights()
+                if len(weights) == 2:
+                    self.output_layers.append((weights[0], weights[1], layer.activation))
+        
+        self.num_steering_actions = num_steering_actions
+        self.num_engine_actions = num_engine_actions
+        self.state_history = []
+        self.action_history = []
+        self.rng = np.random.default_rng(8282)
+
+    def apply_activation(self, x, activation):
+        import tensorflow as tf
+        import numpy as np
+
+        if activation == tf.keras.activations.relu:
+            return np.maximum(0, x)
+        elif activation == tf.keras.activations.tanh:
+            return np.tanh(x)
+        elif activation == tf.keras.activations.sigmoid:
+            return 1 / (1 + np.exp(-x))
+        elif activation == tf.keras.activations.linear:
+            return x
+        elif activation == tf.keras.activations.softmax:
+            # Stable softmax
+            z = x - np.max(x, axis=-1, keepdims=True)
+            e = np.exp(z)
+            return e / np.sum(e, axis=-1, keepdims=True)
+        else:
+            raise NotImplementedError(f"Unsupported activation: {activation}")
+    
+    def eval(self, inputs: list[float], state: list[float]):
+        import numpy as np
+        import math
+
+        # Unpack input vec
+        left_distance = inputs[0]
+        right_distance = inputs[2]
+        # Unpack state vec
+        steering_angle = state[1]
+
+        # Convert inputs and state into single numpy array
+        x = np.array(inputs + state, dtype=np.float32).reshape(1, -1)
+        
+        # Forward pass through hidden layers
+        for W, b, activation in self.hidden_layers:
+            xprime = np.dot(x, W) + b
+            x = self.apply_activation(xprime, activation)
+        
+        # Forward pass through outputs
+        Y = []
+        for W, b, activation in self.output_layers:
+            yprime = np.dot(x, W) + b
+            y = self.apply_activation(yprime, activation)
+            Y.append(y[0])
+
+        engine_action_probs = Y[0]
+
+        # Sample actions
+        engine_action = self.rng.choice(self.num_engine_actions, p=engine_action_probs)
+
+        # Update histories
+        self.state_history.append(inputs + state)
+        self.action_history.append(engine_action)
+
+        engine_power = 0.0
+        steering_power = 0.0
+
+        if engine_action == 0:
+            engine_power = -1.0
+        elif engine_action == 1:
+            engine_power = 0.0
+        else:
+            engine_power = 1.0
+
+        side_distance_diff = left_distance - right_distance
+        steering_power = 0.0
+        side_distance_diff_normalized = max(-1.0, min(1.0, side_distance_diff / 5.0))
+        min_steering_angle = -30.0*(math.pi/180.0)
+        max_steering_angle = 30.0*(math.pi/180.0)
+        target_steering_angle = min_steering_angle + (side_distance_diff_normalized + 1.0) * ((max_steering_angle - min_steering_angle) / 2.0)
+        steering_angle_diff = steering_angle - target_steering_angle
+
+        if steering_angle_diff > 1.0*(math.pi/180.0):
+            steering_power = -1.0
+        elif steering_angle_diff < -1.0*(math.pi/180.0):
+            steering_power = 1.0
+
+        return [engine_power, steering_power]
+
+class NNSteeringAgent:
+    def __init__(
+            self, 
+            model, 
+            num_steering_actions: int
+            ):
+        import tensorflow as tf
+        import numpy as np
+        # Upack model weights
+        # Weights evaluated with numpy for performance
+        layers = model.layers
+
+        self.hidden_layers = []
+        for layer in layers:
+            if isinstance(layer, tf.keras.layers.Dense) and "out" not in layer.name:
+                weights = layer.get_weights()
+                if len(weights) == 2:
+                    self.hidden_layers.append((weights[0], weights[1], layer.activation))
+
+        self.output_layers = []
+        for layer in layers:
+            if isinstance(layer, tf.keras.layers.Dense) and "out" in layer.name:
+                weights = layer.get_weights()
+                if len(weights) == 2:
+                    self.output_layers.append((weights[0], weights[1], layer.activation))
+        
+        self.num_steering_actions = num_steering_actions
+        self.state_history = []
+        self.action_history = []
+        self.rng = np.random.default_rng(8282)
+
+    def apply_activation(self, x, activation):
+        import tensorflow as tf
+        import numpy as np
+
+        if activation == tf.keras.activations.relu:
+            return np.maximum(0, x)
+        elif activation == tf.keras.activations.tanh:
+            return np.tanh(x)
+        elif activation == tf.keras.activations.sigmoid:
+            return 1 / (1 + np.exp(-x))
+        elif activation == tf.keras.activations.linear:
+            return x
+        elif activation == tf.keras.activations.softmax:
+            # Stable softmax
+            z = x - np.max(x, axis=-1, keepdims=True)
+            e = np.exp(z)
+            return e / np.sum(e, axis=-1, keepdims=True)
+        else:
+            raise NotImplementedError(f"Unsupported activation: {activation}")
+    
+    def eval(self, inputs: list[float], state: list[float]):
+        import numpy as np
+
+        # Convert inputs and state into single numpy array
+        x = np.array(inputs + state, dtype=np.float32).reshape(1, -1)
+        
+        # Forward pass through hidden layers
+        for W, b, activation in self.hidden_layers:
+            xprime = np.dot(x, W) + b
+            x = self.apply_activation(xprime, activation)
+        
+        # Forward pass through outputs
+        Y = []
+        for W, b, activation in self.output_layers:
+            yprime = np.dot(x, W) + b
+            y = self.apply_activation(yprime, activation)
+            Y.append(y[0])
+
+        steering_action_probs = Y[0]
+
+        # Sample actions
+        steering_action = self.rng.choice(self.num_steering_actions, p=steering_action_probs)
+
+        # Update histories
+        self.state_history.append(inputs + state)
+        self.action_history.append(steering_action)
+
+        steering_power = 0.0
+
+        if steering_action == 0:
+            steering_power = -1.0
+        elif steering_action == 1:
+            steering_power = 0.0
+        else:
+            steering_power = 1.0
+
+
+        # Unpack input vec
+        forward_distance = inputs[1]
+        # Unpack state vec
+        speed = state[0]
+
+        target_speed = 20*(forward_distance)
+        speed_diff = speed - target_speed
+        engine_power = 0.0
+
+        if speed_diff < 1.0:
+            engine_power = 1.0
+        elif speed_diff > 1.0:
+            engine_power = -1.0
+
+        return [engine_power, steering_power]
+    
---
+++ hcc-simulator/train_nn_steering.py
@@ -0,0 +1,175 @@
+import numpy as np
+import keras
+from keras import ops
+from keras import layers
+import tensorflow as tf
+import agents
+
+# Configuration parameters for the whole setup
+seed = 42
+gamma = 0.99  # Discount factor for past rewards
+max_seconds_per_episode = 60
+max_steps = max_seconds_per_episode*60
+max_episodes = 5
+eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
+    
+# Load baseline checkpoint times
+baseline_checkpoint_times = np.load('baseline_checkpoint_times.npy')
+
+# Setup actor critic network
+num_inputs = 5
+num_steering_actions = 3
+num_hidden = 128
+
+inputs = layers.Input(shape=(num_inputs,))
+common = layers.Dense(num_hidden, activation="relu")(inputs)
+action_engine = layers.Dense(num_steering_actions, activation="softmax", name = "steering_out")(common)
+critic = layers.Dense(1, activation="linear", name="critic_out")(common)
+
+model = keras.Model(inputs=inputs, outputs=[action_engine, critic])
+
+# Train neural network agent
+optimizer = keras.optimizers.Adam(learning_rate=0.01)
+huber_loss = keras.losses.Huber()
+engine_action_probs_history = []
+critic_value_history = []
+rewards_history = []
+episode_count = 0
+
+while episode_count < max_episodes:
+    sim.load_environment("training_environment")
+
+    episode_reward = 0
+    # Create agent and run episode to get states
+    agent = agents.NNSteeringAgent(model, num_steering_actions)
+    sim.print(f"Running steering-only training episode {episode_count + 1}.")
+    handle = sim.run_episode(agent, max_steps)
+
+    while True:
+        if handle.is_done():
+            break
+
+    # Upack results
+    checkpoint_times, terminated, end_step = handle.get_result()
+    state_history = agent.state_history
+    action_history = agent.action_history
+    states_tf = tf.convert_to_tensor(agent.state_history, dtype=tf.float32)
+
+    sim.print(f" Completed in {end_step} steps")
+
+    with tf.GradientTape() as tape:
+        # Use actual model to calculate states, needed to compute gradients
+        action_steering, critic_values = model(states_tf, training=True)
+
+        # Extract chosen action probabilities
+        steering_indices = tf.constant([a for a in agent.action_history], dtype=tf.int32)
+        steering_action_probs_history = tf.gather(action_steering, steering_indices, axis=1, batch_dims=1)
+
+        j = 0 # Checkpoint times history
+
+        for step, (state, action) in enumerate(zip(agent.state_history, agent.action_history)):
+            # Inputs
+            left_distance = state[0]
+            forward_distance = state[1]
+            right_distance = state[2]
+            # State
+            speed = state[3]
+            steering_angle = state[4]
+            # Action
+            steering_action = action
+
+            # Compute steering power
+            steering_power = 0.0
+
+            if steering_action == 0:
+                steering_power = -1.0
+            elif steering_action == 1:
+                steering_power = 0.0
+            else:
+                steering_power = 1.0
+
+            # Define tolerance for "centered"
+            center_tolerance = 0.1
+
+            # Initialize reward
+            reward = 0.0
+
+            # --- Base checkpoint reward ---
+            if j < len(checkpoint_times) and step > checkpoint_times[j]:
+                j += 1
+
+            if j < len(checkpoint_times):
+                baseline_time = baseline_checkpoint_times[j]
+                nn_time = checkpoint_times[j]
+                reward = np.maximum(baseline_time - nn_time, 0)
+
+            side_distance_diff = left_distance - right_distance
+            side_distance_diff_normalized = max(-1.0, min(1.0, side_distance_diff / 5.0))
+            min_steering_angle = -30.0*(np.pi/180.0)
+            max_steering_angle = 30.0*(np.pi/180.0)
+            target_steering_angle = min_steering_angle + (side_distance_diff_normalized + 1.0) * ((max_steering_angle - min_steering_angle) / 2.0)
+            steering_angle_diff = target_steering_angle - steering_angle
+            steering_angle_diff_normalize = (steering_angle_diff)/(max_steering_angle - min_steering_angle)
+
+            # Steering rewards/penalties
+            center_tolerance = 0.1
+
+            # Turning
+            if abs(steering_angle_diff) > 1.0*(np.pi/180.0):
+                if steering_angle < target_steering_angle:
+                    reward += (1 if steering_power == 1 else -1)
+                elif steering_angle > target_steering_angle:
+                    reward += (1 if steering_power == -1 else -1)
+            else:
+                reward += (1 if steering_power == 0 else -1)
+
+            # Append reward
+            rewards_history.append(reward)
+        
+        if terminated:
+            rewards_history[-1] -= 100
+
+        # Calculate expected value from rewards
+        # - At each timestep what was the total reward received after that timestep
+        # - Rewards in the past are discounted by multiplying them with gamma
+        # - These are the labels for our critic
+        returns = []
+        discounted_sum = 0
+        for r in rewards_history[::-1]:
+            discounted_sum = r + gamma * discounted_sum
+            returns.insert(0, discounted_sum)
+
+        # Normalize
+        returns = np.array(returns)
+        returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
+        returns = returns.tolist()
+
+        # Calculating loss values to update our network
+        history = zip(steering_action_probs_history, critic_values, returns)
+        actor_losses = []
+        critic_losses = []
+
+        for log_prob_steering, value, ret in history:
+            # At this point in history, the critic estimated that we would get a
+            # total reward = `value` in the future. We took an action with log probability
+            # of `log_prob` and ended up receiving a total reward = `ret`.
+            # The actor must be updated so that it predicts an action that leads to
+            # high rewards (compared to critic's estimate) with high probability.
+            diff = ret - value
+            log_prob = log_prob_steering
+            actor_losses.append(-log_prob * diff)  # actor loss
+            # The critic must be updated so that it predicts a better estimate of
+            # the future rewards.
+            critic_losses.append(
+                huber_loss(ops.expand_dims(value, 0), ops.expand_dims(ret, 0))
+            )
+
+        # Backpropagation
+        loss_value = tf.add_n(actor_losses) + tf.add_n(critic_losses)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        episode_count += 1
+
+        sim.print(f"rewards = ({episode_reward})")
+    
