822f82c063e6b76c44715260419d675aa65ce358
KylerLimata <kyler.limata@gmail.com>
2025-12-11 17:20:46 -0800

    Increase learning rate

--- hcc-simulator/train_nn_single_critic.py
+++ hcc-simulator/train_nn_single_critic.py
@@ -43,7 +43,7 @@ critic = layers.Dense(1, activation="linear", name="critic_out")(common)
 model = keras.Model(inputs=inputs, outputs=[action_steering, action_engine, critic])
 
 # Train neural network agent
-optimizer = keras.optimizers.Adam(learning_rate=3e-4)
+optimizer = keras.optimizers.Adam(learning_rate=0.01)
 huber_loss = keras.losses.Huber()
 engine_action_probs_history = []
 critic_value_history = []
