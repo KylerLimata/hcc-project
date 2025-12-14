3162ebb273054368f7a2fc6930be7d962e9dd638
KylerLimata <kyler.limata@gmail.com>
2025-12-14 12:25:32 -0800

    Switch baseline back to training_environment_new

--- hcc-simulator/run_baseline.py
+++ hcc-simulator/run_baseline.py
@@ -2,7 +2,7 @@ import agents
 import numpy as np
 
 # Evaluate the baseline agent
-sim.load_environment("training_course_four")
+sim.load_environment("training_environment_new")
 handle = sim.run_episode(agents.BaselineAgent(breaking=False), 60*60)
 sim.print("Running baseline agent.")
 
